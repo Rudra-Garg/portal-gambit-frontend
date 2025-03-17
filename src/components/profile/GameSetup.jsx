0409f5f4bfb59a51a4bed6aace3c9a794adaec32
SnehanshuPal27 <palsnehanshu27@gmail.com>
2025-03-17 19:32:51 +0530

    fixed typo in GameSetup.jsx

--- src/components/profile/GameSetup.jsx
+++ src/components/profile/GameSetup.jsx
@@ -6,7 +6,7 @@ import { auth } from '../../firebase/config';
 import { ref, push, get, update, remove } from 'firebase/database';
 import { database } from '../../firebase/config';
 import PortalChessGame from '../game/PortalChessGame';
-import AvailableGamesComponent from './availableGameComponent';
+import AvailableGamesComponent from './AvailableGameComponent.jsx';
 import { initialBoardSetup } from '../game/chessLogic';
 
 const GameSetup = () => {
