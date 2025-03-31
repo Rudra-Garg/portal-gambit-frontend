82614d18bafd784861dddb89f97b48ad0839cc50
SnehanshuPal27 <palsnehanshu27@gmail.com>
2025-04-01 00:42:40 +0530

    added cloud backend url in config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://portal-gambit-backend-196209545251.asia-south1.run.app';
 
 export { BACKEND_URL };
