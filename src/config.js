d0c463d42f62ff4421667c9124d7fce4a9deaa09
Rudra Garg <121709697+Rudra-Garg@users.noreply.github.com>
2025-04-01 11:41:15 +0530

    Update config.js
    Signed-off-by: Rudra Garg <121709697+Rudra-Garg@users.noreply.github.com>

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://portal-gambit-backend-196209545251.asia-south1.run.app';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080';
 
 export { BACKEND_URL };
